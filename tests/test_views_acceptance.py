5d122a30d6d01e44dd72ed4667a1df2bc600a728
Ryan MacInnis <MacInnis82@gmail.com>
2015-08-30 18:18:01 +0000

    Added some more tests

--- tests/test_views_acceptance.py
+++ tests/test_views_acceptance.py
@@ -18,19 +18,26 @@ class TestViews(unittest.TestCase):
   def setUp(self):
     """ Test Setup """
     self.browser = Browser("phantomjs")
+    self.browser.driver.set_window_size(1440, 900) # set browser size.
     
     # set up the tables in the database
     Base.metadata.create_all(engine)
     
-    # create an example user
-    self.user = models.User(
-      name="Alice",
-      email="alice@example.com",
-      password=generate_password_hash("test")
-    )
-    
+    # create 2 example users
+    self.user = models.User(name="Alice", email="alice@example.com", password=generate_password_hash("test"))
     session.add(self.user)
     session.commit()
+    
+    self.user2 = models.User(name="Ryan", email="macinnis82@gmail.com", password=generate_password_hash("test"))
+    session.add(self.user2)
+    session.commit()
+    
+    # Create some sample posts
+    content = "Acceptance testing content"
+    for i in range (5):
+      post = models.Post(title="Acceptance test post #{}".format(i+1), content=content, author=self.user)
+      session.add(post)
+    session.commit()    
     
     """ gives the ability to start and run other code simultaneously with your own scripts """
     self.process = multiprocessing.Process(target=app.run)
@@ -47,25 +54,49 @@ class TestViews(unittest.TestCase):
     Base.metadata.drop_all(engine)
     self.browser.quit()
     
-  def test_login_correct(self):
-    # self.browser.visit("http://0.0.0.0:8080/login")
+  def login(self, email, password):
     self.browser.visit("http://127.0.0.1:5000/login")
-    self.browser.fill("email", "alice@example.com")
-    self.browser.fill("password", "test")
+    self.browser.fill("email", email)
+    self.browser.fill("password", password)
     button = self.browser.find_by_css("button[type=submit]")
-    button.click()
-    # self.assertEqual(self.browser.url, "http://0.0.0.0:8080/")
+    button.click()    
+    
+  def test_login_correct(self):
+    self.login(email="alice@example.com", password="test")
     self.assertEqual(self.browser.url, "http://127.0.0.1:5000/")
     
   def test_login_incorrect(self):
-    # self.browser.visit("http://0.0.0.0:8080/login")
-    self.browser.visit("http://127.0.0.1:5000/login")
-    self.browser.fill("email", "bob@example.com")
-    self.browser.fill("password", "test")
-    button = self.browser.find_by_css("button[type=submit]")
-    button.click()
-    # self.assertEqual(self.browser.url, "http://0.0.0.0:8080/login")
+    self.login(email="bob@example.com", password="test")
     self.assertEqual(self.browser.url, "http://127.0.0.1:5000/login")
+    
+  # def test_add_post(self):
+  #   self.login(email="alice@example.com", password="test")
+  #   self.browser.visit("http://127.0.0.1:5000/post/add")
+  #   self.browser.fill("title", "new title")
+  #   self.browser.fill("content", "new content")
+  #   button = self.browser.find_by_css("button[type=submit]")
+  #   button.click()
+  #   self.assertEqual(self.browser.url, "http://127.0.0.1:5000/")
+  #   self.assertTrue(self.browser.is_text_present("new title"))
+  #   self.assertTrue(self.browser.is_text_present("new content")) 
+    
+  # def test_edit_post_as_author(self):
+  #   self.login(email="alice@example.com", password="test")
+  #   self.browser.visit("http://127.0.0.1:5000/post/edit/3")
+  #   self.assertEqual(self.browser.url, "http://127.0.0.1:5000/post/edit/3")
+  #   self.browser.fill("title", "edited title")
+  #   time.sleep(2)
+  #   self.browser.fill("content", "edited content")
+  #   button = self.browser.find_by_css("button[type=submit]")
+  #   button.click()
+  #   self.assertEqual(self.browser.url, "http://127.0.0.1:5000/")
+  #   self.assertTrue(self.browser.is_text_present("edited title"))
+  #   self.assertTrue(self.browser.is_test_present("edited content"))
+    
+  def test_edit_post_not_as_author(self):
+    self.login(email="macinnis82@gmail.com", password="test")
+    self.browser.visit("http://0.0.0.0:8080/post/edit/2")
+    self.assertEqual(self.browser.url, "http://127.0.0.1:5000/")  
     
 if __name__ == "__main__":
   unittest.main()
--- tests/test_views_integration.py
+++ tests/test_views_integration.py
@@ -41,13 +41,13 @@ class TestViews(unittest.TestCase):
     with self.client.session_transaction() as http_session:
       http_session["user_id"] = str(self.user.id)
       http_session["_fresh"] = True
-      
+  
   def test_add_post(self):
     self.simulate_login()
     
     response = self.client.post("/post/add", data={
       "title": "Test Post",
-      "content": "Test Content"
+      "content": "Test content"
     })
     
     self.assertEqual(response.status_code, 302)
@@ -57,9 +57,66 @@ class TestViews(unittest.TestCase):
     
     post = posts[0]
     self.assertEqual(post.title, "Test Post")
-    # self.assertEqual(post.content, "<p>Test content</p>\n") --> this test failed
-    self.assertEqual(post.content, "Test Content")
+    # self.assertEqual(post.content, "<p>Test content</p>\n")
+    self.assertEqual(post.content, "Test content")
     self.assertEqual(post.author, self.user)
     
+  # test edit post when user logged in
+  def test_edit_post(self):
+    self.simulate_login()
+    
+    test_post = models.Post(
+      title="Test Post",
+      content="Test Content",
+      author_id=self.user.id
+    )
+    session.add(test_post)
+    session.commit()
+    
+    response = self.client.post("/post/edit/1", data={
+      "title": "Edit Post",
+      "content": "Edit content"
+    })
+    
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(urlparse(response.location).path, "/")
+    posts = session.query(models.Post).all()
+    self.assertEqual(len(posts), 1)
+    
+    post = posts[0]
+    self.assertEqual(post.title, "Edit Post")
+    # self.assertEqual(post.content, "<p>Edit content</p>\n")
+    self.assertEqual(post.content, "Edit content")
+    self.assertEqual(post.author, self.user)
+  
+  # test logged out users cannot delete things
+  def test_delete_post_logout(self):
+    pass
+  
+  # test users cannot delete posts they didn't author
+  def test_delete_post_not_author(self):
+    pass
+  
+  # test user can delete their own posts
+  def test_delete_post(self):
+
+    test_post = models.Post(
+      title="Test Post",
+      content="Test Content",
+      author_id=self.user.id
+    )
+    session.add(test_post)
+    session.commit()
+    post_id=test_post.id
+    
+    self.simulate_login()
+    response = self.client.post("/post/delete/{}".format(post_id))
+
+    """ self.assertEqual(response.status_code, 302) AssertionError: 405 != 302 """
+    self.assertEqual(response.status_code, 302)
+    self.assertEqual(urlparse(response.location).path, "/")
+    posts = session.query(models.Post).all()
+    self.assertEqual(len(posts), 0)
+  
 if __name__ == "__main__":
   unittest.main()
